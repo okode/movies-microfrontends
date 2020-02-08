f0111489684809a4a5b8b3a7461e3bce94e8b77b
Pedro Jorquera <pjorquera@okode.com>
2020-02-08 16:50:31 +0100

    Refactor

--- okode-movies-spa/projects/okode-movies/src/lib/movie-list-page/movie-list-page.component.ts
+++ okode-movies-spa/projects/okode-movies/src/lib/movie-list-page/movie-list-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'okode-movie-list-page',
   templateUrl: './movie-list-page.component.html',
   styleUrls: ['./movie-list-page.component.scss']
 })
-export class MovieListPageComponent implements OnInit {
+export class MovieListPageComponent {
 
-  constructor() { }
+  readonly movies = [ 'Interstellar', 'Blade Runner', 'Star Wars', 'The Matrix', 'Aliens' ];
 
-  ngOnInit() {
+  constructor(private navCtrl: NavController) { }
+
+  goToMovieDetail(movie: string) {
+    this.navCtrl.navigateForward('movies/detail');
   }
 
 }
