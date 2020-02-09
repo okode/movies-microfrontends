d24e3d1512e6d126ae550822d00240a0f7600357
Pedro Jorquera <pjorquera@okode.com>
2020-02-09 18:17:28 +0100

    Added list and detail components

--- ionic4-spa/src/app/app-routing.module.ts
+++ ionic4-spa/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'detail', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
