2cfa94ede132bb27ce62fb2eda71ccfd105bb5be
Pedro Jorquera <pjorquera@okode.com>
2020-02-11 20:57:56 +0100

    Added In App Browser

--- ionic3-app/src/pages/home/home.ts
+++ ionic3-app/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private iab: InAppBrowser) {
 
+  }
+
+  openInAppBrowser() {
+    this.iab.create('https://www.google.com', '_blank').show();
   }
 
 }
