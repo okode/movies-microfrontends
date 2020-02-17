60f8821cf8f00e9c9847d060b79fb9f22eebefad
Pedro Jorquera <pjorquera@okode.com>
2020-02-17 19:54:52 +0100

    Cosmetics

--- ionic3-app/src/pages/home/home.ts
+++ ionic3-app/src/pages/home/home.ts
@@ -8,11 +8,19 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 export class HomePage {
 
   constructor(private iab: InAppBrowser) {
-
+    
   }
 
   openInAppBrowser() {
-    this.iab.create('https://www.google.com', '_blank').show();
+    this.iab.create('https://www.google.com', '_blank',{
+      hidenavigationbuttons: 'yes',
+      hideurlbar: 'yes',
+      location: 'no',
+      hidden: 'yes',
+      footer: 'no',
+      toolbar: 'no',
+      hidespinner: 'yes'
+    }).show();
   }
 
 }
